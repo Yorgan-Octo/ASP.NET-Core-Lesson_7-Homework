d2af7fbb91518f25778a7b4d1bf2a95f0bda0100
Artem Pr <yorgan.info@gmail.com>
2023-08-25 12:32:05 +0300

    Добавьте файлы проекта.

---
+++ ASP.NET-Core-Lesson_7-Homework/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using ASP.NET_Core_Lesson_7_Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_Core_Lesson_7_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(FormData formData)
+        {
+            Debug.WriteLine("Данные поля First: " + formData.First);
+            Debug.WriteLine("Данные поля Second: " + formData.Second);
+            Debug.WriteLine("Данные поля Count: " + formData.Count);
+            return View();
+        }
+    }
+}
---
+++ ASP.NET-Core-Lesson_7-Homework/Models/FormData.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_Lesson_7_Homework.Models
+{
+    public class FormData
+    {
+
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+
+    }
+}
